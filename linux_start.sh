0944452561f7d8d025c3c232a5903aa566696042
Stephane Boudier <stephane.boudier.etu@univ-lille.fr>
2025-03-30 09:41:06 +0200

    Modifier linux_start.sh - vérification du dossier nodemon

--- linux_start.sh
+++ linux_start.sh
@@ -1,7 +1,21 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 WEB_GAME_PATH="$SCRIPT_DIR/web_game_displayer"
 REFLECTOR_PATH="$SCRIPT_DIR/reflector-linux-x64"
+RAILROAD_PATH="$SCRIPT_DIR/railroad"
 GAMEMASTER_PATH="$SCRIPT_DIR/gamemaster"
+
+check_and_install_npm() {
+    local dir="$1"
+    if [ ! -d "$dir/node_modules" ]; then
+        echo "Dossier node_modules introuvable dans $dir, exécution de npm install..."
+        cd "$dir" || exit
+        npm install
+        sleep 5
+    fi
+}
+
+check_and_install_npm "$WEB_GAME_PATH"
+check_and_install_npm "$RAILROAD_PATH"
 
 cd "$WEB_GAME_PATH" || exit
 echo "Lancement de nodemon..."
@@ -35,6 +49,13 @@ fi
 
 echo "Port récupéré : $PORT"
 
+cd "$RAILROAD_PATH" || exit
+echo "Lancement de l'arbitre..."
+node --watch referee.js | tee railroad.log &
+ARBITRE_PID=$!
+
+sleep 3
+
 echo "Attente de 60 secondes avant de lancer GameMaster..."
 sleep 60
 
@@ -43,5 +64,4 @@ echo "Lancement de GameMaster avec l'adresse IP : $IP et le port : $PORT..."
 runghc GameMaster.hs "$IP" "$PORT" &
 GAMEMASTER_PID=$!
 
-
-wait $NPM_PID $REFLECTOR_PID $GAMEMASTER_PID
+wait $NPM_PID $REFLECTOR_PID $ARBITRE_PID $GAMEMASTER_PID
